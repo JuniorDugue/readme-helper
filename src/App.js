df1c284ec779f2a2eb8e5b2bebf4e2b7cc3aad6d
juniordugue <jrdugue@gmail.com>
2020-12-24 06:25:27 -0500

    installed styled components and react markdown packages, added font awesome, and few styled components for the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  outline: 1px solid red;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 1.5rem;
+  font-weight: 700;
+  font-family: "Lato", sans-serif;
+  margin-bottom: 1em;
+`;
+
+const EditorContainer = styled.div`
+  outline: 1px solid blue;
+  height: 100%;
+  width: 100%;
+`
 
 function App() {
   return (
-    <div className="container">
-      <h1>ReadME Helper</h1>
-    </div>
+    <Container>
+      <Title>
+        <h1>ReadME Helper</h1>
+      </Title>
+      <EditorContainer>
+
+      </EditorContainer>
+    </Container>
   );
 }
 
